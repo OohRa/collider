4d88e318d2761516de684628acbfd393414f1833
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-01-29 19:06:35 -0500

    Master

---
+++ include/bitboards.h
@@ -0,0 +1,8 @@
+#ifndef BITBOARDS_H
+#define BITBOARDS_H
+//bitboards.h
+
+//Functions
+void setBit( int sq, U64 &bbAddress );
+
+#endif 
---
+++ src/bitboards.cpp
@@ -0,0 +1,14 @@
+//bitboards.cpp
+
+#include "primer.h"
+#include "bitboards.h"
+
+void setBit( int sq, U64 &bbAddress )
+{
+	U64 setBit = 1;
+	U64 bb = bbAddress;
+	setBit <<= sq;
+	bb = bb | setBit;
+	bbAddress = bb;
+}
+ 
