4578faf726f351097da712d2f1e7acd8d80ea241
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-02-26 11:13:59 -0500

    Cleaned up functions. Added comments.

--- src/board.cpp
+++ src/board.cpp
@@ -3,29 +3,33 @@
 #include "primer.h"
 #include "board.h"
 
-
+//Takes a square and returns the piece value
 int getPiece( int sq ){
 	return board.sq[sq];
 }	
 
+//Take a square and returns the color of the piece on that square
 int getColor( int sq ){
 	int sqVal;
 	sqVal = getPiece( sq );
 
-	if( sqVal >= wqR && sqVal <= wPh ) return WHITE;
-	else if( sqVal >= bqR && sqVal <= bPh ) return BLACK;
-	else if( sqVal == 0 )
+	//Check for color
+	if( sqVal >= wqR && sqVal <= wPh )      //White pieces
+		 return WHITE;
+	else if( sqVal >= bqR && sqVal <= bPh ) //Black Pieces
+		 return BLACK;
+	else if( sqVal == EMPTY ) 		//Empty squares
 		return NONE;
-	else return OFFBOARD;
+	else return OFFBOARD;			//General else
 }
 
+//Takes a square and returns type of piece on that square
 int getType ( int sq ){
 	int type, piece;
-	
 	piece = getPiece( sq );
 
 	//Check what type the piece is.
-	switch ( piece ){
+	switch ( piece ){		//Switch from display function
 		case wqR:
 		case wkR:
 		case bqR:
@@ -42,11 +46,17 @@ int getType ( int sq ){
 		case  bQ: type = QUEEN; break;
 		case  wK:
 		case  bK: type = KING; break;
-		default: type = PAWN; break;
+		default:  type = PAWN; break;
 	}
-	if( piece == 0 ) type = EMPTY;
 	
-	if( piece == -1 ) type = OFFBOARD;
+	/* Check afterwards for empty and offboard squares
+	   to use default option in switch for pawns */
+	if( piece == EMPTY )
+		 type = NOTYPE;
+	
+	if( piece == -1 )
+		 type = OFFBOARD;
+
 	return type;
 }
 
