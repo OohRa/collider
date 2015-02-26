a5fff333f23ec91f56ac3c9fbe0266ba04a7ff74
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-02-26 11:34:31 -0500

    bitboards.cpp will remain mostly unchanged until bitboards are added to the actual board representation.

--- src/bitboards.cpp
+++ src/bitboards.cpp
@@ -1,8 +1,11 @@
 //bitboards.cpp
+// Will add in actual bitboard functionality
+// after completion of basic chess engine
 
 #include "primer.h"
 #include "bitboards.h"
 
+/* Used to set a certain bit in a bitboard */
 void setBit( int sq, U64 &bbAddress ){
 	U64 setBit = 1;
 	U64 bb = bbAddress;
