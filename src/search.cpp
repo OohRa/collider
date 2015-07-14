ab3866449d1338ca4b0ab8f3bc05ae58c79165fb
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-07-14 11:32:15 -0400

    Outputs algebraic notation and cleaner output.

--- src/search.cpp
+++ src/search.cpp
@@ -12,22 +12,29 @@
 #include <chrono>
 
 int leaf = 0;
-int grandDepth = 8;
+int alpha = -99999;
+int beta = 99999;
+//grandDepth = 7;
 void engine(){
 	int score, bestMove, bestScore;
 	int depth = grandDepth;
+	alpha = -99999;
+	beta = 99999;
 	life = 0;
 	score = 0;
-	int alpha = -99999;
-	int beta = 99999;
-	bestScore = -99999;
+	bestScore = alpha;
 	bestMove = 0;
-	
+/*	
 	if( checkCheck() )
-		moveGen(board.mL, false, true);
+		//Remove until implemented cleaning movelist
+		//moveGen(board.mL, false, true);
+		
 
 	else
 		moveGen( board.mL );
+*/
+
+	moveGen( board.mL );
 	//Time def	
 	auto t1 = std::chrono::high_resolution_clock::now();
 	typedef std::chrono::milliseconds ms;
@@ -37,6 +44,11 @@ void engine(){
 	for( int i = 0; i < board.mL.size(); i++ ){
 
 		setMove(board.mL, i);
+
+		std::cout << "AlphaBeta move is: ";
+		dispMove(board.mL[i]);
+		std::cout << "\n";
+
 		makeMove();
 
 		if( checkCheck() ){
@@ -46,11 +58,13 @@ void engine(){
 	
 		changeSide();
 
+		//Set the moves to display while the bot searches
 		if( slowFlag )
 			displayAll();
+
+		//Node incrementer
 		life++;	
 		
-		std::cout << "AlphaBeta move is: " << board.mL[i] << "\n";
 
 		//Call to negaMax 
 		score = -negaMax( -beta, -alpha, depth - 1 );
@@ -95,29 +109,47 @@ void engine(){
 	leaf = 0;
 
 	std::cout << "Index is: " << bestMove << "\n";
-	std::cout << "The best move is: " << board.mL[bestMove] << "\n";
-	std::cout << "With a score of: " << bestScore << "\n";
+	std::cout << "Depth is " << grandDepth << "\n";
 	std::cout << "movelist size is: " << board.mL.size() << "\n";
+	std::cout << "The best move is: ";
+	dispMove(board.mL[bestMove]);
+	nl(1);
+	std::cout << "With a score of: " << bestScore << "\n";
 	std::cout << "Current score is: ";
 
 	setMove(board.mL, bestMove);
 	makeMove();
+
+	//Test for current position check
+	if( checkCheck() )
+		std::cout << "You're in check man, and you already moved, that's illegal!\n";
+
 	changeSide();
+
+	//Test for current position check
+	if( checkCheck() )
+		std::cout << "You're in check!\n";
+
+	//debugAll();
 	std::cout << -eval() << "\n";
 }
 
 int negaMax( int alpha, int beta, int depth ){
 	int score;	
+
+	//Think about intializing this vector with a size to increase speed
 	std::vector<int> mL;
+	mL.reserve(40);
+
 	if( depth == 0 ){
-		//changeSide();
+		//score = eval();
 		score = quiescence( alpha, beta );
-		//changeSide();
-		//std::cout << "Current score is: " << best << "\n";
 		return score;
 	}
 	moveGen(mL);
+
 //	debugAll();
+
 	for( int i = 0; i < mL.size(); i++ ){
 		setMove(mL, i);
 		makeMove();
@@ -126,7 +158,7 @@ int negaMax( int alpha, int beta, int depth ){
 			unmakeMove();
 			continue;
 		}
-	
+
 		changeSide();
 
 		if( slowFlag )
@@ -168,6 +200,7 @@ int quiescence(int alpha, int beta){
 	}
 	//changeSide();
 	std::vector<int> mL;
+	mL.reserve(10);
 	//std::cout << "In quiescence search.\n";
 	moveGen(mL, true);
 	//std::cout << "Searching: " << mL.size() << " moves.\n";
