25307dc2094ca26399ed3e3d3e240d94f5ee5913
DylanJamesKolbBond <djk13b@my.fsu.edu>
2015-07-14 11:30:20 -0400

    Moved material count to move.cpp. Add bitboard passed pawn evaluation, open files, batteries, isolated pawns, and doubled pawns.

--- src/eval.cpp
+++ src/eval.cpp
@@ -1,4 +1,4 @@
-//eval.cpp
+
 #include "eval.h"
 #include <primer.h>
 #include "board.h"
@@ -6,9 +6,7 @@
 #include "debug.h"
 #include "sdl.h"
 
-int evaluate(){
-
-	static int flip[64] = {  56, 57, 58, 59, 60, 61, 62, 63,
+	int flip[64] = {         56, 57, 58, 59, 60, 61, 62, 63,
 				 48, 49, 50, 51, 52, 53, 54, 55,
 				 40, 41, 42, 43, 44, 45, 46, 47,
 				 32, 33, 34, 35, 36, 37, 38, 39,
@@ -18,52 +16,61 @@ int evaluate(){
 				  0,  1,  2,  3,  4,  5,  6,  7 };
 
 
-	static int pawnTable[64] = {   0,   0,   0,   0,   0,   0,   0,   0,
-				      10,  10,  10, -30, -40,  10,  10,  10,
-				       5,   0,   5,   0,   0,  -5,  10,  15,
-				       0,   0,  10,  25,  30,  10,   5,   0,
-				       0,   0,  10,  35,  35,  25,  25,   0,
-				      10,  15,  20,  40,  40,  40,  40,   0,
+	int pawnTable[64] = {          0,   0,   0,   0,   0,   0,   0,   0,
+				       0,   0, -15, -20, -20, -10,   0,   0,
+				       5,   5,   5,   5,   5,   5,  10,  15,
+				       5,   5,  20,  30,  35,  10,   5,   5,
+				      17,  17,  10,  35,  35,  25,  25,  10,
+				      15,  35,  40,  40,  40,  40,  30,  15,
 				      80,  80,  80,  80,  80,  80,  80,  80,
-				     100, 100, 100, 100, 100, 100, 100, 100 };
+				       0,   0,   0,   0,   0,   0,   0,   0 };
 
-	static int knightTable[64] = { -30, -20, -20, -20, -20, -20, -30, -30,
-				       -20, -10, -10,  10,  10, -10, -10, -10,
-				       -20,   5,  25,   5,   5,  30,  10, -10,
-				        10,   5,  10,  15,  20,  20,  20,  10,
-				         0,  15,  10,  25,  25,  20,  15,  10,
-					 5,  10,  15,  20,  25,  20,  15,  10,
-				         0,   5,  20,  10,  10,  25,   5,  25,
-			                 0,   0,   0,   0,   0,   0,   0,   0 };
+	int pawnTable2[64] = {          0,   0,   0,   0,   0,   0,   0,   0,
+				      -20, -20, -20, -20, -20, -20, -20, -20,
+				       5,   5,   5,   5,   5,   5,  10,  15,
+				      15,  15,  10,  25,  30,  10,  15,  25,
+				      17,  17,  50,  35,  35,  25,  55,  10,
+				      45,  60,  60,  60,  60,  60,  60,  45,
+				      60,  80,  80,  80,  80,  80,  80,  60,
+				       0,   0,   0,   0,   0,   0,   0,   0 };
+	
+	int knightTable[64] = {        -30, -20,   0,   0,   0, -10, -30, -30,
+				         0,   0,   0,  10,  10,   0,   0,   0,
+				         0,   5,  15,   5,   5,  15,  10,   0,
+				         0,   5,  10,  15,  20,  20,  20,   0,
+				         0,  15,  10,  25,  25,  20,  15,   0,
+					 0,  10,  15,  20,  25,  20,  15,   0,
+				         0,   5,  20,  10,  10,  25,   5,   5,
+			               -30,   0,   0,   0,   0,   0,   0, -30 };
 
-	static int bishopTable[64] = {   0,   0, -20, -10, -10, -20,   0,   0,
-				         5,  15,  10,   5,  20,   0,  85,   5,
-				       -10,  10,  10,   5,   5,   0,   5, -10,
-				        10,  -5,  10,  10,  10,  20,  10,  -5,
-				        -5,  20,   5,  15,  15,  10,  30,  10,
-				         5,   0,  10,   5,  15,   5,   5,   5,
-			               -10,  10,   0,   0,   0,  20,  10,  15,
+	int bishopTable[64] = {          0,   0, -20,   0,   0, -20,   0,   0,
+				         5,  10,   0,   5,  10,   0,  15,   5,
+				         0,   0,   5,  15,  10,   0,   0,   0,
+				         0,   5,  20,   5,   5,  15,   5,   0,
+				         5,  20,   5,  15,  15,  10,  20,   0,
+				         5,   0,  10,   5,  15,   5,   5,   0,
+			                 0,   0,   0,   0,   0,   0,   0,   0,
 				         0,   0,   0,   0,   0,   0,   0,   0 };   
 			 
-	static int rookTable[64] = {    0,   0,   0,  40,  40,   0,   0,   0,
-				      -15,   0,   0,   0,   0,   0,   0,   0,
+	int rookTable[64] = {           0,  10,  20,  40,  40,  20,  10,   0,
+				        0,   0,   0,   0,   0,   0,   0,   0,
 				        0,   0,   0,   0,  10,  15,  10,  10,
 				        0,   0,   0,   0,   0,   0,   0,   0,
 				        0,   0,   0,   0,   0,   0,   0,   0,
-				        0,   0,   0,   0,  10,  15,  10,   5,
-				       60,  65,  65,  70,  70,  60,  65,  60,
-				        5,  10,  10,  10,  10,  10,  10,  10 };
+				        0,   0,   0,   0,   0,   5,   0,   5,
+				       50,  50,  50,  50,  50,  50,  50,  50,
+				       20,  20,  20,  20,  20,  20,  20,  20 };
 
-	static int queenTable[64] = {   0,   0,   5,  20,   0,   0,   0,   0,
-					0, -10,  10,  10,   5,   0, -10,   0,
-				        0,   0,   0,   5,   0,   5,  10,   5,
+	int queenTable[64] = {          0,   0,   5, -10,   0,   0,   0,   0,
+					0,   0,  10,  10,  10,   0,   0,   0,
+				        0,   0,   0,   5,   0,   0,   0,   0,
 					0,   0,   0,   0,   0,   5,   0,   5,
 					0,  10,   0,   0,   0,   5,   5,   0,
 					0,   0,   0,   0,   0,   0,   0,   0,
 				        5,   5,   5,   5,   5,  10,  10,  10,
 				       10,  10,  10,  10,  10,  10,  15,  20 };
 
-	static int kingTable1[64] = {  0,  20, -10, -20, -40, -90, 100,  50,
+	int kingTable1[64] = {        20,  40,  20, -20, -40, -50,  50,  40,
 				       0,   0,   0, -30, -80, -20,  10,  15,
 				     -20, -30, -40, -50, -50, -40, -30, -20,
 				     -50, -50, -50, -50, -50, -50, -50, -50,
@@ -71,149 +78,220 @@ int evaluate(){
 			   	     -50, -50, -50, -50, -50, -50, -50, -50,
 				     -50, -50, -50, -50, -50, -50, -50, -50,
 			  	     -50, -50, -50, -50, -50, -50, -50, -50 }; 
-/*
-	static int kingTable2[64] = {  -50, -40, -30, -20, -20, 10, -10, -20,
-				       -40, -30, -20,   0,  15, 10,   5,   0,
-				       -10,   0,   5,  10,  15, 10,   0,   0,
-				       -10,   0,   5,  25,  25, 10,   0,   0,
-				       -10,   0,   5,  15,  15, 10,   0,   0,
-					 0,   0,  10,  10,  10, 10,   0,   0,
-				        -5,  -5,   5,   5,   5,  5,   0,   0,
+
+	int kingTable2[64] = {         -50, -40, -10,   0,   0,  5, -40, -20,
+				        10,  10,  15,  20,  15, 15,  10,   5,
+				        15,  15,  15,  20,  25, 10,  15,  10,
+				        10,  10,  15,  25,  25, 15,  10,  10,
+				        10,  10,  15,  25,  25, 15,  30,  10,
+					10,  10,  10,  25,  20, 15,  30,  10,
+				        15,  15,  35,  35,  35, 35,  30,  10,
 					 0,   0,   0,   0,   0,  0,   0,   0 };
-*/
+int evaluate(){
+
+	
 	//Add total material count to score and piece table values
-	int score = 0, type, sq;
+	int score = 0, type = 0, sq = 0;
 	int piece = (board.side == WHITE) ? wqR: bqR;
-	for( int i = 1; i < 17; i++ ){
+	int bPair = 0;
+	bool endgame = false;
+	int king = (board.side == WHITE) ? wK: bK;
+	int mod = (board.side == WHITE) ? 1: -1;
+	int opp = getOpp();
+
+	//Material Count
+	score += board.material[board.side];
+
+	//Determine endgame
+	if( score < 102000 ){
+		endgame = true;
+		std::cout << "Endgame!\n";
+	}
+
+	piece = (board.side == WHITE) ? wqR: bqR;
+	//Extra Count
+	for( int i = 1; i < 17; ++i ){
 		if( pce[piece].life == false ){
-			if( piece == wK || piece == bK )
-				score -= 999999;
 			piece++;
 			continue;
 		}
-		 else{
-			score += pce[piece].value;
-			sq = indexA.sq[pce[piece].pos];
-			type = getType(pce[piece].pos);
-			if( ply < 10 && pce[piece].moved > 1 )
-				score -= 40;	
-			if( board.side == BLACK )
-				sq = flip[sq];
-			if( type == PAWN ){
+		sq = indexA.sq[pce[piece].pos];
+		type = getType(pce[piece].pos);
+		if( board.side == BLACK )
+			sq = flip[sq];
+		if( type == PAWN ){
+			if( endgame )
+				score += pawnTable2[sq];
+			else
 				score += pawnTable[sq];
+		}
+		else if( type == KNIGHT ){
+			score += knightTable[sq];
+		}
+		else if( type == BISHOP ){
+			score += bishopTable[sq];
+			bPair++;
+			if( bPair == 2 )
+				score += 50;
+		}
+		else if( type == ROOK ){
+			score += rookTable[sq];
+		}
+		else if( type == QUEEN ){
+			score += queenTable[sq];
+			if( ply < 10 && pce[piece].moved > 1 ){
+				score -= 50;
 			}
-			else if( type == KNIGHT ){
-				score += knightTable[sq];
+		}
+		else if( type == KING ) {
+			if(board.castled[board.side]){
+				std::cout << "Castled\n";
+				score += 50;
 			}
-			else if( type == BISHOP ){
-				score += bishopTable[sq];
-			}
-			else if( type == ROOK ){
-				score += rookTable[sq];
-			}
-			else if( type == QUEEN ){
-				score += queenTable[sq];
-				if( ply < 10 && pce[piece].moved > 1 )
-					score -= 100;
-			}
-			else if( type == KING ) {
-				if( pce[piece].pos % 10 == 7 || pce[piece].pos % 10 == 3 )
-					score += 50;
+			if( endgame )
+				score += kingTable2[sq];
+			else 
 				score += kingTable1[sq];
-			}
-			else{
-/*
-				std::cout << "What piece is this? " << piece << "\n";
-				std::cout << "Type is: " << type << "\n";
-				std::cout << "Position is: " << pce[piece].pos << "\n";
-				std::cout << "Life is: " << pce[piece].life << "\n";
-				//debugAll();
-				//SDL_Delay(100000);
-*/
-			}
+		}
+		else{
+			std::cout << "What\n";
+		}
 		
+		piece++;	
+		}
+
+	//King Safety
+	if(board.castled[board.side]){
+		int test = 0;
+		sq = pce[king].pos + mod * 9; 
+		for( int i = 0; i < 3; ++i ){
+			if( getType(sq) == PAWN && getColor(sq) == board.side ){
+				score += 10;
+				test += 10;
+			}
+			sq += mod;
+		}
+		std::cout << "King Safety score = " << test << "\n";
+	
+	}
+	
+	//Passed pawns with advanced bitboards
+	piece = (board.side == WHITE) ? wPa: bPa;	
+	for( int i = piece; i < piece + 7; ++i ){
+		if( pce[i].life == true ){
+			if( (bb.passPwn[board.side][pce[i].pos-31] & bb.pawns[opp]) == 0 ){
+				bbDisp( bb.passPwn[board.side][pce[i].pos - 31] );
+				bbDisp( bb.pawns[opp] );
+				bbDisp( pce[i].bitboard );
+				std::cout << "Side is " << board.side << "\n";
+				std::cout << "Piece is: " << i << "\n";
+				std::cout << "Passed pawn!\n";
+				score += 50;
+			}
+		}
+	}
+
+	//Doubled pawns check
+	int dblPawns[8] = {0,0,0,0,0,0,0,0};
+	int testSq = 0;
+
+	piece = (board.side == WHITE) ? wPa: bPa;	
+/*
+	//Check pawns on files	
+	for( int i = piece; i < piece + 7; ++i ){
+		testSq = pce[i].pos % 10;
+		if( pce[i].life == true )
+			dblPawns[testSq - 1] += 1;
+
+		//Passed and isolated pawns
+		if( pce[i].pos % 10 == 1 ){
+			//Passed
+			//if( bb.pawns[getOpp()
+			if(( bb.pawns[getOpp()] & bb.file[1]) == 0 ){
+				std::cout << "Passed a-file pawn.\n";
+				score += 40;
+			}
+			//Isolated
+			if(( bb.pawns[board.side] & bb.file[1]) == 0 ){
+				std::cout << "Isolated a-file pawn.\n";
+				score -= 20;
+			}
 			
 		}
-		piece++;
-	}
-/*
-	//Penalize early queen movement
-	if( ply < 10 && getType(board.toSq) == QUEEN && getColor(board.toSq) == board.side){
-		std::cout << "Moving queen early!\n";
-		score -= 200;
-	}
-*/
-/*
-	//Penalize moving a piece more than once in the opening
-	if( ply < 10 && pce[getPiece(board.toSq)].moved > 1 && getColor(board.toSq) == board.side ){
-		//std::cout << "Moving a piece more than once!\n";
-		score -= 200;
-	}
-*/
-/* Remove bitboards until implemented in makemove
-	//Check for rook open files
-	piece = (board.side == WHITE) ? wqR: bqR;
-	generateBB();
-	U64 bitMask = 0;
-	int change = 7;
-	for( int i = 0; i < 2; i++ ){
-		if( pce[piece].life == false )
-			continue;
-		sq = indexA.sq[pce[piece].pos];
-		bitMask++;
-		bitMask <<= sq;
-		bitMask = bb.ownPieces ^ bitMask;
-		//std::cout << "bitmask is: " << std::bitset<64>(bitMask) << "\n";
-		//std::cout << "File bb is: " << std::bitset<64>(bb.file[(pce[piece].pos % 10 - 1)]) << "\n";
-		if( (bb.file[(pce[piece].pos % 10 - 1)] & bitMask) == 0 ){
-			//std::cout << "Open file!\n";
-			score += 200;
-		}
-
-		piece += change;			
-		bitMask = 0;
-	}
-
-*/
-	//Check for open ranks (later)
-/* Fix this!
-	//Check for passed pawns
-	piece = (board.side == WHITE) ? wPa: bPa;
-	for( int i = 0; i < 8; i++ ){
-		if( pce[piece].life == false )
-			continue;
-		if( pce[piece].pos % 10 == 1 ){
-			if( (bb.file[FILE_B] & bb.oppPawn) == 0 ){
-				//std::cout << "Passed pawn!\n";
-				score += 90;
+		else if( pce[i].pos % 10 == 8 ){
+			//Passed
+			if(( bb.pawns[getOpp()] & bb.file[7]) == 0 ){
+				std::cout << "Passed g-file pawn.\n";
+				score += 40;
 			}
-		}
-		else if( pce[piece].pos % 10 == 8 ){
-			if( (bb.file[FILE_G] & bb.oppPawn) == 0 ){
-				std::cout << "Passed pawn!\n";
-				score += 90;
+			//Isolated
+			if(( bb.pawns[board.side] & bb.file[7]) == 0 ){
+				std::cout << "Isolated h-file pawn.\n";
+				score -= 20;
 			}
 		}
 		else{
-			if( ((bb.file[pce[piece].pos % 10] & bb.oppPawn) == 0) && ((bb.file[pce[piece].pos % 10 + 2] & bb.oppPawn) == 0) ){
-				std::cout << "Passed pawn!\n";
-				score += 90; 
+			//Passed
+			if( (bb.pawns[getOpp()] & bb.file[pce[i].pos % 10 - 1]) == 0){
+				if( (bb.pawns[getOpp()] & bb.file[pce[i].pos % 10 + 1]) == 0){
+					std::cout << "Passed pawn.\n";
+					score += 50;
+				}
+			}
+			//Isolated
+			if(( bb.pawns[board.side] & bb.file[pce[i].pos % 10 - 1]) == 0){
+				if( (bb.pawns[board.side] & bb.file[pce[i].pos % 10 + 1]) == 0){
+					std::cout << "Isolated pawn.\n";
+					score -= 20;
+				}
 			}
 		}
-		piece++;
 	}
 */
-
-/*
-	//Add bonus for castling
-	if( abs(board.toSq - board.frSq) == 2 && getType(board.toSq) == KING ){
-		//std::cout << "Castle Bonus!!!\n";
-		score += 500;
+	//Subtract points for doubled pawns
+	for( int i = 0; i < 8; ++i ){
+		if( dblPawns[i] > 1 ){
+			//std::cout << "Double pawns " << dblPawns[i] << "\n";
+			//std::cout << "Score is " << score;
+			score -= (dblPawns[i] - 1) * 20;
+			//std::cout << "Score is " << score;
+			dblPawns[i] = 0;
+			//std::cout << "Side is " << board.side << "\n";
+			std::cout << "Double pawns subtraction\n";
+		}
 	}
-*/	
+ 
+	//Open files check
+	piece = (board.side == WHITE) ? wqR: bqR;
+	for( int i = piece; i <= piece + 7; i += 7 ){
+		if( pce[i].life == false ){
+			++i;
+			continue;
+		}
+		else{
+			testSq = (pce[i].pos % 10) - 1;
+			//std::cout << "Testsq is " << testSq << "\nPiece is " << i << "\n";
+			//std::cout << "Side is " << board.side << "\n";
+			if( (bb.file[testSq] & bb.pawns[board.side]) == 0 ){
+				score += 20;
+				std::cout << "Rook on open file yes.\n";
+			}
+		}
+	}
+
+	//Battery check
+	piece = (board.side == WHITE) ? wqR: bqR;
+	if( pce[piece].pos % 10 == pce[piece + 7].pos % 10 ){
+		score += 20;
+		std::cout << "Rook battery\n";
+	}
+
+	//
+
 	return score;
 }
 
+/*
 void generateBB(){
 	int piece, square, sq64;
 	U64 bitMask = 0;
@@ -246,12 +324,14 @@ void generateBB(){
 	}
 	//std::cout << "Opponent pawn bitboard is: " << std::bitset<64>(bb.oppPawn) << "\n";
 }	
-
+*/
 int eval(){
 	int one = 0, two = 0;
 	one = evaluate();
+	//one -= 25;
 	changeSide();
 	two = evaluate();
 	changeSide();
 	return (one - two);
 }
+
